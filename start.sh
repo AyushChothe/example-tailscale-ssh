39ac914f5ac78eddb5e921b3f07bac486009d90a
Ayush Chothe <40957514+AyushChothe@users.noreply.github.com>
2024-08-23 22:58:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,6 +11,6 @@ trap _term TERM
 
 /app/tailscaled -state=mem: -statedir=/var/lib/tailscale -socket=/var/run/tailscale/tailscaled.sock -tun userspace-networking -socks5-server localhost:1055 &
 TAILSCALE_DAEMON_PID=$!
-/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${KOYEB_APP_NAME}-${KOYEB_SERVICE_NAME}
+/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${PYRITE_APP_NAME}-${PYRITE_SERVICE_NAME}
 
 wait
